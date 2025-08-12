197409e3f75729dcb6abc3b3f4dd46598d491271
Guy Barros <guy@Hashicorp.com>
2025-08-12 22:55:16 +0100

    added cnpj, removed vars

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
-output "enconded_value" {
-  value = data.vault_transform_encode.test.batch_results
+output "cpf_enconded_value" {
+  value = data.vault_transform_encode.cpf-test.batch_results
+}
+
+output "cnpj_enconded_value" {
+  value = data.vault_transform_encode.cnpj-test.batch_results
 }
